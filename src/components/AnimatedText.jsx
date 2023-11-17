4420f2c422c4da1bafbc1d7091692e606aadcb25
gene howell <gene.howell9@gmail.com>
2023-11-16 23:27:00 -0600

    Added alt colors and added starter temp for about page

--- src/components/AnimatedText.jsx
+++ src/components/AnimatedText.jsx
@@ -4,7 +4,7 @@ const AnimatedText = ({ text, className = "" }) => {
   return (
     <div className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden">
       <h1
-        className={`inline-block w-full text-dark font-bold capitalize text-8xl ${className}`}
+        className={`inline-block w-full text-dark font-bold capitalize text-7xl ${className}`}
       >
         {text.split(" ").map((word, index) => (
           <span key={word + "-" + index} className="inline-block">
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,30 +1,34 @@
+"use client"
+
 import Head from "next/head"
 import Image from "next/image"
 import profilePic from "../../public/images/profile/profilePic4.png"
 import Link from "next/link"
+import { useState } from "react"
 
 import AnimatedText from "@/components/AnimatedText"
 import { LinkArrow } from "@/components/icons"
 import Layout from "@/components/Layout"
 
 export default function Home() {
-  const CustomLink = ({ href, title, className = "" }) => {
-    const router = router()
-    return (
-      <CustomLink
-        href={href}
-        className={`${className} relative group text-slate-900`}
-      >
-        {title}
-        <span
-          className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 text-slate-900 ${
-            router.asPath === href ? "w-full" : "w-0"
-          }`}
-        >
-          &nbsp;
-        </span>
-      </CustomLink>
-    )
+  const [isDarkMode, setDarkMode] = useState(false)
+
+  const toggleDarkMode = () => {
+    setDarkMode(!isDarkMode)
+  }
+
+  const lightModeColors = {
+    purple: "bg-purple-300",
+    blue: "bg-blue-300",
+    orange: "bg-orange-300",
+    green: "bg-green-300",
+  }
+
+  const darkModeColors = {
+    neonPurple: "bg-indigo-500 text-indigo-800",
+    neonBlue: "bg-teal-500 text-teal-800",
+    neonOrange: "bg-amber-500 text-amber-800",
+    neonGreen: "bg-emerald-500 text-emerald-800",
   }
 
   return (
@@ -36,45 +40,76 @@ export default function Home() {
           content="Gene Howell - Digital media specialist skilled in video, graphics, web development, and radio production. Transforming ideas into captivating digital experiences for maximum impact. Let's create something extraordinary!"
         />
       </Head>
-      <main className="flex items-center text-slate-900 w-full min-h-screen">
+      <main
+        className={`flex items-center ${
+          isDarkMode ? "dark:text-white dark:bg-gray-900" : "text-slate-900"
+        } w-full min-h-screen`}
+      >
         <Layout className="pt-0">
           <div className="flex items-center justify-between w-full">
             <div className="w-1/2 relative">
-              <div className="absolute top-32 left-52 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
-              <div className="absolute top-20 left-5 w-72 h-72 bg-blue-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
-              <div className="absolute -bottom-8 left-40 w-72 h-72 bg-orange-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
-              <div className="absolute -bottom-5 left-15 w-72 h-72 bg-green-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-5000"></div>
+              <div
+                className={`absolute top-32 left-52 w-72 h-72 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob ${
+                  isDarkMode
+                    ? darkModeColors.neonPurple
+                    : lightModeColors.purple
+                }`}
+              ></div>
+              <div
+                className={`absolute top-20 left-5 w-72 h-72 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000 ${
+                  isDarkMode ? darkModeColors.neonBlue : lightModeColors.blue
+                }`}
+              ></div>
+              <div
+                className={`absolute -bottom-8 left-40 w-72 h-72 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000 ${
+                  isDarkMode
+                    ? darkModeColors.neonOrange
+                    : lightModeColors.orange
+                }`}
+              ></div>
+              <div
+                className={`absolute -bottom-5 left-15 w-72 h-72 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-5000 ${
+                  isDarkMode ? darkModeColors.neonGreen : lightModeColors.green
+                }`}
+              ></div>
               <Image
                 src={profilePic}
                 alt="Gene Howell"
                 className="w-full h-auto rounded-full relative z-10"
               />
             </div>
-            <div className="w-1/2 flex flex-col items-center self-center text-slate-900">
+            <div
+              className={`w-1/2 flex flex-col items-center self-center ${
+                isDarkMode ? "dark:text-white" : "text-slate-900"
+              }`}
+            >
               <AnimatedText
-                text="Hello there! I'm Gene, your personal Digital Media
-                Specialist"
-                className="!text-4xl !text-left animate-fade-in-down text-slate-900"
+                text="Hello there! I'm Gene, your personal Digital Media Specialist"
+                className="!text-4xl !text-left animate-fade-in-down"
               />
-              <p className="my-4 text-base font-medium text-slate-900">
+              <p className="my-4 text-base font-medium">
                 From the very beginning, my journey in the realm of technology
                 began with web development. However, as I progressed through
                 college, my horizons expanded, and I discovered my
-                &ldquo;deep-seated&rdquo; passion for programming, graphic
-                design, video production, and radio.
+                &quot;deep-seated&quot; passion for programming, graphic design,
+                video production, and radio.
               </p>
-              <p className="my-4 text-base font-medium text-slate-900">
+              <p className="my-4 text-base font-medium">
                 Today, I bring together my diverse expertise to create
                 captivating digital experiences that seamlessly integrate the
                 worlds of web development, programming, graphic design, video
-                production, and radio. Welcome to my portfolio site, where you
-                can explore the culmination of my multifaceted skills and
+                production, and radio. Welcome to my online portfolio, where you
+                will explore the culmination of my multifaceted skills and
                 projects.
               </p>
               <div className="flex items-center self-start mt-2">
                 <Link
                   href="/Resume.pdf"
-                  className="flex items-center bg-slate-900 text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark hover:scale-[1.1] ease-in duration-300"
+                  className={`flex items-center ${
+                    isDarkMode
+                      ? "dark:bg-slate-900 dark:text-light"
+                      : "bg-slate-900 text-light"
+                  } p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark hover:scale-[1.1] ease-in duration-300`}
                   download={true}
                 >
                   Resume
@@ -82,11 +117,21 @@ export default function Home() {
                 </Link>
                 <Link
                   href="mailto:genehowell9@gmail.com"
-                  className="contactAnimate ml-4 text-lg font-medium capitalize text-slate-900 underline hover:underline-offset-2 hover:ease-out"
+                  className={`contactAnimate ml-4 text-lg font-medium capitalize ${
+                    isDarkMode
+                      ? "dark:text-white underline"
+                      : "text-slate-900 underline"
+                  } hover:underline-offset-2 hover:ease-out`}
                 >
                   Contact Me
                 </Link>
               </div>
+              <button
+                onClick={toggleDarkMode}
+                className="mt-4 text-lg font-medium capitalize underline hover:underline-offset-2 hover:ease-out"
+              >
+                Toggle Dark Mode
+              </button>
             </div>
           </div>
         </Layout>
