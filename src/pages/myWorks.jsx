8c3fd341cb6d5e7b23402d82e3f0dee9e07b36d2
gene howell <gene.howell9@gmail.com>
2024-01-01 15:36:18 -0600

    got rid of featured project component as I do not need for this page

--- src/pages/myWorks.jsx
+++ src/pages/myWorks.jsx
@@ -1,6 +1,5 @@
 import AnimatedText from "@/components/AnimatedText"
 import Layout from "@/components/Layout"
-import { GithubIcon } from "@/components/icons"
 import Head from "next/head"
 import Image from "next/image"
 import Link from "next/link"
@@ -10,45 +9,6 @@ import project2 from "../../public/images/projects/graphicDesign.jpg"
 import project3 from "../../public/images/projects/videoProduction.jpg"
 import project4 from "../../public/images/projects/AudioProduction.jpg"
 import TransitionEffect from "@/components/TransitionEffect"
-
-const FeaturedProject = ({ type, title, summary, img, link, github }) => {
-  return (
-    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12">
-      <Link
-        href={link}
-        target="_blank"
-        className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
-        priority
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
-      >
-        <Image src={img} alt={title} className="w-full h-auto" />
-      </Link>
-      <div className="w-1/2 flex flex-col items-start justify-between pl-6">
-        <span className="text-primary font-medium text-xl">{type}</span>
-        <Link
-          href={link}
-          target="_blank"
-          className="hover:underline underline-offset-2"
-        >
-          <h2 className="my-2 w-full text-left text-4xl font-bold">{title}</h2>
-        </Link>
-        <p className="my-2 font-medium text-dark">{summary}</p>
-        <div className="mt-2 flex items-center">
-          <Link href={github} target="_blank" className="w-10">
-            <GithubIcon />
-          </Link>
-          <Link
-            href={link}
-            target="_blank"
-            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold"
-          >
-            Visit My Project
-          </Link>
-        </div>
-      </div>
-    </article>
-  )
-}
 
 const Project = ({ title, img, link, alt }) => {
   return (
