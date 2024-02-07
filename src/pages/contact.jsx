1ade0fe08c889e2811da259f0a80a409ff4f032f
Gene Howell <gene.howell9@gmail.com>
2024-02-06 21:18:36 -0600

    re-aligned the contact page, and changed the text

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,6 +1,8 @@
 import Layout from '@/components/Layout'
 import TransitionEffect from '@/components/TransitionEffect'
 import Head from 'next/head'
+import Image from 'next/image'
+import profilePic from "../../public/images/profile/profilePic4.png"
 import Link from 'next/link'
 import React from 'react'
 
@@ -17,20 +19,28 @@ const contact = () => {
             <TransitionEffect />
             <main className="flex items-center text-dark w-full min-h-screen dark:text-light md:pt-4">
                 <Layout className="pt-0">
-                    <div className="flex items-center justify-between w-full lg:flex-col ">
+                    <div className="flex items-center justify-between w-full lg:flex-col">
                         {/* Contact Form Section */}
-                        <div className="w-1/2 flex flex-col lg:w-full px-20 py-0">
-                            <h1 className="text-2xl mb-3">Contact Gene Howell - Your Digital Media Specialist</h1>
-                            <h1 className='text-xl mb-3'>Let's Create Something Extraordinary</h1>
-                            <p className='text-lg mb-3'>Hello there! If you're looking for a seasoned Digital Media Specialist to bring your creative ideas to life, you're in the right place! I offer a wide range of services, including <Link className='hover:underline hover:font-bold' href="/webProjects">web development</Link>, <Link className='hover:underline hover:font-bold' href="/">programming</Link>, <Link className='hover:underline hover:font-bold' href="/">graphic design</Link>, <Link className='hover:underline hover:font-bold' href="/" >video production</Link>, and <Link className='hover:underline hover:font-bold' href="/" >radio</Link>.</p>
-                            <p className='text-lg mb-3'>Whether you have a specific project in mind or you're seeking guidance on how digital media can elevate your brand, I'm here to help. Let's collaborate and transform your vision into a captivating digital experience.</p>
-                            <h1 className='text-2xl mb-3'>Let's Connect!</h1>
-                            <p>Ready to embark on a creative journey together? Connect with me via email or download my resume to learn more about what I bring to the table. I look forward to hearing from you and creating something extraordinary!</p>
+                        <div className="w-full lg:w-1/2 flex flex-col items-center px-20 py-0">
+                            <div className="flex items-center justify-center mb-8">
+                                <Image
+                                    src={profilePic}
+                                    alt="Gene Howell"
+                                    className="w-1/2 h-auto rounded-full relative z-50 lg:flex-col lg:w-full md:pb-1 md:w-full lg:mb-16"
+                                    priority
+                                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 20vw, 20vw"
+                                />
+                            </div>
+                            <div className='text-left'>
+                                <h1 className="text-2xl mb-3">Contact Gene Howell - Your Digital Media Specialist</h1>
+                                <p className='text-lg mb-3'>Hello! I'm Gene Howell, your experienced Digital Media Specialist. From web development to graphic design and video production, I offer a wide range of services. Let's collaborate to transform your vision into a captivating digital experience.</p>
+                                <h1 className='text-2xl mb-3'>Let's Connect!</h1>
+                                <p className='text-lg mb-3'>Reach out via email or download my resume to explore how I can contribute to your projects. Excited to create something extraordinary together!</p>
+                            </div>
                         </div>
 
                         {/* Contact Information Section */}
-                        <div className="w-1/2 relative dark:z-10">
-                            {/* Include your existing contact information section here */}
+                        <div className="w-full lg:w-1/2 relative dark:z-10">
                             <div className="my-4">
                                 <form action="#" method="post" className="space-y-4">
                                     <div>
